7e8577c2acf35575b0ddc6b9c5408c3712163726
Mingliang Tao <tmldebug@gmail.com>
2018-11-18 00:31:21 +0800

    fix: file path string (#8)

--- src/__tests__/schema-validator.test.js
+++ src/__tests__/schema-validator.test.js
@@ -3,16 +3,16 @@ const fs = require('fs-extra');
 
 it('schema validation succeeds', async () => {
     expect.assertions(1);
-    const schema = await fs.readJson('.\\schemas\\config-schema.json').then();
-    const example = await fs.readJson('.\\examples\\config-example.json');
+    const schema = await fs.readJson('./schemas/config-schema.json').then();
+    const example = await fs.readJson('./examples/config-example.json');
     const validation = await schemaValidator(schema, example);
     expect(validation.valid).toBe(true);
 });
 
 it('schema validation fails', async () => {
     expect.assertions(1);
-    const schema = await fs.readJson('.\\schemas\\config-schema.json').then();
-    const negativeExample = await fs.readJson('.\\examples\\config-negative-example.json');
+    const schema = await fs.readJson('./schemas/config-schema.json').then();
+    const negativeExample = await fs.readJson('./examples/config-negative-example.json');
     const validation = await schemaValidator(schema, negativeExample);
     expect(validation.valid).toBe(false);
 });
