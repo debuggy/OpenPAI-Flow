8d082cb6f23d59c28a00e7c00da33267fc0a9053
Qinzheng Sun <Qinzheng.Sun@microsoft.com>
2018-12-06 08:30:46 +0000

    Merged PR 22: add pip install step; organize directory structure; add cli.js
    add pip install step; organize directory structure; add cli.js

--- scripts/debug.js
+++
@@ -1,9 +0,0 @@
-const dockerfileGenerator = require('../lib/index')
-const fs = require('fs-extra')
-const path = require('path')
-
-const dockerfilePath = path.join(__dirname, '../out/dockerfile')
-fs.readJson('./examples/install-conda.json')
-  .then(example => dockerfileGenerator(example, dockerfilePath))
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
